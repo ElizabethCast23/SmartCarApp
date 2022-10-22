b7922688e354b08f314b8dedceee0a4b4782ba10
Elizabeth Castro <elizacast2302@gmail.com>
2022-10-22 01:18:52 -0500

    Adm_nav

---
+++ app/src/main/java/dev/example/smartcarapp/MenuActivityAdm.kt
@@ -0,0 +1,35 @@
+package dev.example.smartcarapp
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import dev.example.smartcarapp.databinding.ActivityMenuAdmBinding
+
+class MenuActivityAdm : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMenuAdmBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMenuAdmBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_menu_adm)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.NavUserProfile, R.id.NavDashboard, R.id.NavTravel, R.id.NavBookUser
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
